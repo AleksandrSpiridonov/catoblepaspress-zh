48c45b8159fa843308fc51b9a3d5c429794664ac
AleksandrSpiridonov <11279575+AleksandrSpiridonov@users.noreply.github.com>
2026-09-10 22:21:36 +0300

    Add Hindi language link and remove reading mode button

--- plugins/language-switcher/dist/components/index.d.ts
+++ plugins/language-switcher/dist/components/index.d.ts
@@ -3,6 +3,7 @@ import type { QuartzComponentConstructor } from "@quartz-community/types"
 interface Options {
   englishBaseUrl: string
   russianBaseUrl: string
+  hindiBaseUrl: string
 }
 
 export declare const LanguageSwitcher: QuartzComponentConstructor<Options>
